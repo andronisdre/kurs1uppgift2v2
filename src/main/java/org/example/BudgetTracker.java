76466fe9e978ce1a902193d68ccae0bb61ca2589
andronisdre <andrebulow13@gmail.com>
2023-10-30 14:14:09 +0100

    in the middle of adding comments, soon finished with them

--- src/main/java/org/example/BudgetTracker.java
+++ src/main/java/org/example/BudgetTracker.java
@@ -6,27 +6,37 @@ import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class BudgetTracker {
+    //I declare all these fields here so that they can be reached in the whole BudgetTracker class
     public static Scanner scanner = new Scanner(System.in);
+    /*I use a GregorianCalendar to make a String DayMonthYear which will become the Date in Income and Expense.
+    I also use it for the month field in income and expense*/
     private static GregorianCalendar gregorianCalendar = new GregorianCalendar();
+    //I use a boolean called keepGoing to loop the program in case of incorrect input
     private static boolean keepGoing = true;
     private static IncomeStorage incomeStorage = new IncomeStorage();
     private static ExpenseStorage expenseStorage = new ExpenseStorage();
 
     public static void budgetTracker() throws IOException {
+        //I read in the files of respective Map interface
         incomeStorage.readFile(false, false);
         expenseStorage.readFile(false, false);
         System.out.println("Hello, do you wish to handle incomes or expenses?");
         System.out.println("Press 1 for income options and 2 for expense options");
+        //Uses keepGoing in a while loop so that if the input is incorrect you will be given the prompt to write a new input
+        //only if int choice is 1 or 2 will the while loop end
         while (keepGoing) {
+            //if you input a non-integer it will be caught by the try catch
             try {
                 int choice = scanner.nextInt();
                 System.out.println("You chose option: " + choice);
                 if (choice == 1) {
                     incomeChoices(choice);
+                    //sets keepGoing to false
                     keepGoing = false;
                 } else if (choice == 2) {
                     expenseChoices(choice);
                     keepGoing = false;
+                    //doesnt set keepGoing to false
                 } else System.out.println("You need to choose 1 or 2! Choose again");
             } catch (Exception exception) {
                 System.out.println("You need to write an integer!");
@@ -39,6 +49,7 @@ public class BudgetTracker {
     public static void incomeChoices(int choice) throws IOException {
         System.out.println("What option regarding incomes do you want to look at?");
         System.out.println("1: add incomes, 2: remove incomes, 3: change incomes, 4: show all incomes, 5: show incomes subtracted by expenses, 6: search for an income, 7: show incomes in specified month");
+        //I declare variables here so that they can be used in the whole method
         String transactionTitle;
         double transactionAmount;
         Income income;
@@ -46,14 +57,23 @@ public class BudgetTracker {
         String DayMonthYear = ("Date added: month: " + (gregorianCalendar.get(Calendar.MONTH) + 1) + ", day: " + gregorianCalendar.get(Calendar.DATE) + ", year: " + gregorianCalendar.get(Calendar.YEAR));
         choice = scanner.nextInt();
         System.out.println("Chosen: " + choice);
+        //while loop similar to that of budgetTrackers while loop
         while (keepGoing) {
+            /*I could have added a try catch here as well so that if an incorrect input was given the program would
+            loop this while loop instead of the one in budgetTracker method. I decided against it since that would
+            imply that there's while loops and try catches at every input of the program, which would be a lot of code
+            that clutters it up*/
             switch (choice) {
                 case 1:
                     System.out.println("Write title of income");
                     transactionTitle = scanner.next();
                     existing = new Income(transactionTitle);
+                    //in the add method, if the incomeList Map interface already contains a key identical to the
+                    //one given by the user, they will have to write a new input
                     if (incomeStorage.getIncomeList().containsKey(existing.getTitle())) {
                         System.out.println("The title of this income already exists! please write a unique title!");
+                        //instead of typing ____ repeatedly I use a String method which repeats the character for as many times you want
+                        //I use _ to separate the message that input was incorrect and the output that gives you a list of keys
                         System.out.println("_".repeat(50));
                         incomeStorage.readFile(true, false);
                         System.out.println("Write an income unique to the ones listed above");
@@ -68,12 +88,14 @@ public class BudgetTracker {
                     }
                 case 2:
                     incomeStorage.readFile(true, false);
+                    //if there are no Incomes in incomeStorage, keepGoing will have been set to false and end the program with the break;
                     if (!keepGoing) {
                         break;
                     }
                     System.out.println("Type one of the keys in the list above to remove that income from the list");
                     transactionTitle = scanner.next();
                     existing = new Income(transactionTitle);
+                    //reverse of the if else statement in case 1:
                     if (incomeStorage.getIncomeList().containsKey(existing.getTitle())) {
                         incomeStorage.removeFile(existing);
                         keepGoing = false;
@@ -102,6 +124,7 @@ public class BudgetTracker {
                     System.out.println("_".repeat(50));
                     break;
                 case 4:
+                    //I comment on these methods in IncomeStorage.java
                     incomeStorage.readFile(true, true);
                     keepGoing = false;
                     break;
@@ -222,10 +245,12 @@ public class BudgetTracker {
         }
     }
 
+    //used to give access for respective Storages to set keepGoing
     public static void setKeepGoing(boolean keepGoing) {
         BudgetTracker.keepGoing = keepGoing;
     }
 
+    //used to give access for respective Storages to get keepGoing
     public static boolean isKeepGoing() {
         return keepGoing;
     }
--- src/main/java/org/example/IncomeStorage.java
+++ src/main/java/org/example/IncomeStorage.java
@@ -16,17 +16,24 @@ public class IncomeStorage {
     public IncomeStorage() {
     }
 
+    //used to give access of incomeList to BudgetTracker
     public Map<String, Income> getIncomeList() {
         return incomeList;
     }
 
+    //boolean andList is used to tell readFile whether it should also print out a list of Incomes
+    //boolean andValues is used to also print out the values of each Income
     public void readFile(boolean andList, boolean andValues) throws IOException {
         Type type = new TypeToken<Map<String, Income>>(){}.getType();
         Reader reader = new FileReader(new File(fileName));
         incomeList = gson.fromJson(reader, type);
         if (andList) {
+            /*boolean exists is used so that if there are no incomes in the list exists will stay false
+            instead of being assigned the value true in the for loop. If false, keepGoing will be set to false.
+            I do this so that if you try for example delete incomes without any existing in the list you will be told so*/
             boolean exists = false;
             System.out.println("Income List:");
+            //enhanced for loop. for as many keys that exist in incomeList it will loop and print the name of the keys
             for(String name : incomeList.keySet()) {
                 System.out.print("Key: " + name); if (andValues) System.out.print(incomeList.get(name));
                 System.out.println();
@@ -57,7 +64,9 @@ public class IncomeStorage {
     }
 
     public void changeFile(Income existing, Income income) throws IOException {
-        //incomeList.replace(existing.getTitle(), income);
+        /*First I had "incomeList.replace(existing.getTitle(), income);"
+        but i could not figure out how to get the key to change along with the title, so I changed it
+        to first remove the existing Income and then add the new Income*/
         incomeList.remove(existing.getTitle());
         incomeList.put(income.getTitle(), income);
         FileWriter fw = new FileWriter(new File(fileName));
